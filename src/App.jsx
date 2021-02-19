33c7814475353a223f751eada3fa08240e33d797
Evgeniy Shigartsov <evgeniy.shigartsov2@gmail.com>
2021-02-19 19:26:36 +0200

    add proxy

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-/* eslint-disable import/no-named-as-default */
 import React from 'react'
-import BannerSlider from './components/BannerSlider/BannerSlider'
+import { BannerSlider } from './components/BannerSlider/BannerSlider'
 
 function App() {
   return (
