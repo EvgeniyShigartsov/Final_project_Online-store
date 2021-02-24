cd960d796b9a7a3d09b96a4e9cf9ba203a05d6ee
Pavlo Iermakov <ptopdruk@gmail.com>
2021-02-24 21:10:03 +0200

    new footer with changes

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render } from '@testing-library/react'
 import { Provider } from 'react-redux'
+import { HashRouter as Router } from 'react-router-dom'
 import { store } from './store/index'
 import App from './App'
 
@@ -24,7 +25,9 @@ test('App render test', () => {
 
   render(
     <Provider store={store}>
-      <App />
+      <Router>
+        <App />
+      </Router>
     </Provider>
   )
 })
