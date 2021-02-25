7ba3fa222218b213f2b076c410363348b4f5f8ae
Nadiia1803 <nadiia.stoiko@gmail.com>
2021-02-25 21:33:49 +0200

    cange app test

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render } from '@testing-library/react'
+import {HashRouter as Router} from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store } from './store/index'
 import App from './App'
 
 test('App render test', () => {
   render(
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <Router>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Router>
   )
 })
