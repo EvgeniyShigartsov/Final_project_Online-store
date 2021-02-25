9c6fd81704fb0638ca0205d4c6d37d5182eedd28
Nadiia1803 <nadiia.stoiko@gmail.com>
2021-02-25 21:06:36 +0200

    add changes at App.test

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -3,12 +3,15 @@ import '@testing-library/jest-dom/extend-expect'
 import { render } from '@testing-library/react'
 import { Provider } from 'react-redux'
 import { store } from './store/index'
+import { HashRouter as Router} from 'react-router-dom'
 import App from './App'
 
 test('App render test', () => {
   render(
+    <Router>
     <Provider store={store}>
       <App />
     </Provider>
+    </Router>
   )
 })
