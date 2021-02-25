29f44e9cc5980a7cc2b7a61eee20b24f88b07d3c
Evgeniy Shigartsov <evgeniy.shigartsov2@gmail.com>
2021-02-25 16:19:41 +0200

    working on responsive

--- src/components/CreateCustomerPage/SignUpBox/Form/SignUpForm.jsx
+++ src/components/CreateCustomerPage/SignUpBox/Form/SignUpForm.jsx
@@ -1,6 +1,10 @@
+/* eslint-disable prefer-const */
+/* eslint-disable no-unused-vars */
+/* eslint-disable prefer-promise-reject-errors */
 /* eslint-disable react/jsx-props-no-spreading */
-import React from 'react'
-import { Form, Input } from 'antd';
+import React, { useState } from 'react'
+import { Input, Col } from 'antd';
+import StyledFrom from './StylesSignUpForm'
 import 'antd/dist/antd.css'
 import Button from '../../../common/Buttons/Button'
 
@@ -8,11 +12,21 @@ const SignUpForm = () => {
   const onFinish = (values) => {
     console.log(values);
   };
-        
+  
+  const formLayout = {
+    labelCol: {
+      span: 10,
+      offset: 0
+    },
+    wrapperCol: {
+      span: 14,
+    },
+    
+  }
+  
   const passwordMatchValidator = ({ getFieldValue }) => {
     const isPasswordsMatch = (_, value) => {
       if (getFieldValue('password') !== value) {
-        // eslint-disable-next-line prefer-promise-reject-errors
         return Promise.reject('Passwords is not match.')
       }
       return Promise.resolve()
@@ -22,74 +36,110 @@ const SignUpForm = () => {
     }
   }
     
-  const layout = {
-    labelCol: {
-      span: 8,
-    },
-    wrapperCol: {
-      span: 16,
-    },
-  };
   return (
-    <Form
-      {...layout}
-      name="sign-up-form"
-      initialValues={{
-        remember: true,
-      }}
-      onFinish={onFinish}
-    >
-      <Form.Item
-        label="Email"
-        name="email"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your email.',
-          },
-        ]}
+    <Col span={24}>
+      <StyledFrom
+        {...formLayout}
+        name="sign-up-form"
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={onFinish}
       >
-        <Input />
-      </Form.Item>
 
-      <Form.Item
-        label="Password"
-        name="password"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your password.',
-          },
-          {
-            message: 'Password length must be at least 8 symbols',
-            min: 8
-          },
-        ]}
-      >
-        <Input.Password />
-      </Form.Item>
-      <Form.Item
-        label="Confrim password"
-        name="confrim-password"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your password.',
-          },
-          {
-            message: 'Password length must be at least 8 symbols',
-            min: 8
-          },
-          passwordMatchValidator,
-        ]}
-      >
-        <Input.Password />
-      </Form.Item>
+        <StyledFrom.Item
+          label="Name"
+          name="name"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your name.',
+            },
+          ]}
+        >
+          <Input />
+        </StyledFrom.Item>
 
-      <Form.Item>
-        <Button type="submit" width={133} height={38}>Sign Up</Button>
-      </Form.Item>
-    </Form>
+        <StyledFrom.Item
+          label="Surename"
+          name="surename"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your Surename.',
+            },
+          ]}
+        >
+          <Input />
+        </StyledFrom.Item>
+
+        <StyledFrom.Item
+          label="Login"
+          name="login"
+          rules={[
+            {
+              required: true,
+              message: 'Please set your login.',
+            },
+          ]}
+        >
+          <Input />
+        </StyledFrom.Item>
+      
+        <StyledFrom.Item
+          label="Email"
+          name="email"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your email.',
+            },
+          ]}
+        >
+          <Input />
+        </StyledFrom.Item>
+
+        <StyledFrom.Item
+          label="Password"
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your password.',
+            },
+            {
+              message: 'Password length must be at least 8 symbols',
+              min: 8
+            },
+          ]}
+        >
+          <Input.Password />
+        </StyledFrom.Item>
+
+        <StyledFrom.Item
+          label="Confrim password"
+          name="confrim-password"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your password.',
+            },
+            {
+              message: 'Password length must be at least 8 symbols',
+              min: 8
+            },
+            passwordMatchValidator,
+          ]}
+        >
+          <Input.Password />
+        </StyledFrom.Item>
+
+        <StyledFrom.Item>
+          <Button type="submit" width={133} height={38}>Sign Up</Button>
+        </StyledFrom.Item>
+
+      </StyledFrom>
+    </Col>
   )
 }
 
---
+++ src/components/CreateCustomerPage/SignUpBox/Form/StylesSignUpForm.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+import { Form } from 'antd'
+
+const StyledFrom = styled(Form)`
+    .ant-form-item {
+        margin-bottom: 10px !important;
+        
+        .ant-form-item-label {
+        padding: 0 !important;
+        }
+    }
+`
+export default StyledFrom
