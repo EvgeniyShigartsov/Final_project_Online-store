428595b5fa92f8d2476601e22464b68fa4ed64e3
Evgeniy Shigartsov <evgeniy.shigartsov2@gmail.com>
2021-02-25 18:40:27 +0200

    upd  link

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { NavLink, Route } from 'react-router-dom'
 import CreateCustomerPage from './components/CreateCustomerPage/CreateCustomerPage'
+import { BannerSlider } from './components/BannerSlider/BannerSlider'
+import { NewProductsSlider } from './components/NewProductsSlider/NewProductsSlider'
 
 function App() {
   return (
-    <CreateCustomerPage />
+    <div>
+      <NavLink to="/signup">Sign Up</NavLink>
+
+      <BannerSlider />
+      <NewProductsSlider />
+      
+      <Route exact path="/signup">
+        <CreateCustomerPage />
+      </Route>
+    </div>
   )
 }
 
--- src/components/CreateCustomerPage/CreateCustomerPage.jsx
+++ src/components/CreateCustomerPage/CreateCustomerPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import { Container } from '../common/Container'
 import { FormContainer, StyledPageHeader } from './StylesCreateCustomer'
 import SignUpBox from './SignUpBox/SignUpBox'
@@ -6,6 +7,7 @@ import SignUpBox from './SignUpBox/SignUpBox'
 const CreateCustomerPage = () => (
   <Container>
     <StyledPageHeader>Customer Registration</StyledPageHeader>
+    <Link to="/">Homepage</Link>
     <FormContainer>
       <SignUpBox />
     </FormContainer>
