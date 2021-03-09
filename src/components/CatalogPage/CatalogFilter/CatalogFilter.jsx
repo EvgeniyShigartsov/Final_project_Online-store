9eec78e6a3aba2f79a781ec4a3e2140365183835
Vlad Baryshpolets <vladikvladvita@gmail.com>
2021-03-09 17:33:37 +0200

    filterComponentDone

--- src/components/CatalogPage/CatalogFilter/CatalogFilter.jsx
+++ src/components/CatalogPage/CatalogFilter/CatalogFilter.jsx
@@ -1,17 +1,20 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable react/prop-types */
 import React, {useRef, useState} from 'react'
-import styled, {css} from 'styled-components';
-//
+
 // eslint-disable-next-line no-unused-vars
 import PropTypes from 'prop-types';
 import {
   Checkbox,
-  Menu, Form,
-  InputNumber, Button
+  Button, Menu
 } from 'antd';
-import { forDesktop, forTablet} from '../../../styles/mediaBreakPoints';
 import close from '../../../images/filter/close.svg';
+// styles
+import {
+  StyledForm, StyledMenuItem, Wrapper, FilterTitle,
+  FormItemWrapper, FromTag, ToTag, InputFrom, InputTo,
+  CloseBtn, StyledMenu, LI, AlignBtn, AllBrandsTitle,
+  GridFilterPart, ContainerBrandsImg, WishList, NoWishItem,
+  WishItem, ContainerImage
+} from './StylesCatalogfilter';
 import {checkPriceValues, checkCurrent} from '../../../utils/checkPriceValues';
 // img
 import imageBrands1 from '../../../images/filter/imageBrands1.svg';
@@ -24,7 +27,10 @@ import styl from '../../../images/filter/styl.svg';
 
 const { SubMenu } = Menu;
 
-const CatalogFilter = ({showFilter, setShowFilter, setFilter}) => {
+const CatalogFilter = ({
+  showFilter, setShowFilter, setFilter
+}) => {
+  // eslint-disable-next-line no-unused-vars
   const [wish, setWish] = useState(true); // temporaly for wisth list
   const { current } = useRef({
     brand: [],
@@ -65,6 +71,7 @@ const CatalogFilter = ({showFilter, setShowFilter, setFilter}) => {
       });
     }
   }
+
   return (
     <StyledForm onFinish={onFinish}>
       <Wrapper showFilter={showFilter}>
@@ -149,220 +156,10 @@ const CatalogFilter = ({showFilter, setShowFilter, setFilter}) => {
     </StyledForm>
   );
 }
-const StyledForm = styled(Form)`
-  background: #F5F7FF;
-  @media(min-width: 591px) {
-      min-width: 27%;
-  } 
-  @media(min-width: ${forDesktop.minWidth}px) {
-      min-width: 20%;
-  } 
-`;
+CatalogFilter.propTypes = {
+  showFilter: PropTypes.bool.isRequired,
+  setShowFilter: PropTypes.func.isRequired,
+  setFilter: PropTypes.func.isRequired
+}
 
-const StyledMenuItem = styled(Menu.Item)`
-  @media(min-width: ${forDesktop.minWidth}px) {
-    padding-left: 20px !important;
-  } 
-  @media(min-width: ${forTablet.minWidth}px) {
-    padding: 0px 0px 0px 10px !important;
-    font-size: 2px !important;
-  } 
-`;
-const Wrapper = styled.aside`
-    @media(max-width: 590px) {
-      position: absolute;
-      top: 0%;
-      left: -100%;
-      width: 100%;
-      height: 100%;
-      background: #ECECEC;
-      transition: all 0.5s ease-out;
-      opacity: 0;
-      z-index: 10;
-      ${(props) => (props.showFilter && css`
-        left: 0%;
-        opacity: 1;
-      `)}
-  }
-`;
-
-const FilterTitle = styled.div`
-  @media(max-width: 591px) {
-    height: 30px;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 25px 35px;
-    background: #ECECEC;
-      h3{
-      font-size: 20px;
-      font-weight: bold;
-    }
-  }
-  @media(min-width: 592px) {
-    padding: 15px 20px;
-    justify-content: center;
-    width: 100%;
-    h3{
-      font-size: 21px;
-      font-weight: bold;
-      text-align: center;
-    }
-  }
-`;
-const FormItemWrapper = styled(Form.Item)`
-  @media(max-width: 591px) {
-    text-align: right !important;
-    padding-right: 20px !important;
-    position: relative;
-  }
-  @media(min-width: 592px) {
-    padding-left: 20px !important;
-  }
-`;
-const FromTag = styled.h6`
-  position: absolute;
-  top: 35px;
-  left: 30px;
-  @media(min-width: 591px) {
-    display: none;
-  }
-`;
-const ToTag = styled.h6`
-  position: absolute;
-  top: 35px;
-  left: 30px;
-  @media(min-width: 591px) {
-    display: none;
-  }
-`;
-const InputFrom = styled(InputNumber)`
-  @media(max-width: 591px) {
-    width: 70% !important;
-    border: 0.5px solid rgba(0,0,0,0.5) !important;
-  }
-  @media(min-width: 592px) {
-    width: 90% !important;
-
-  }
-`;
-const InputTo = styled(InputNumber)`
-  @media(max-width: 591px) {
-    width: 70% !important;
-    border: 0.5px solid rgba(0,0,0,0.5) !important;
-  }
-  @media(min-width: 592px) {
-    width: 80% !important;
-    margin-left: 15px;
-  }
-`;
-const CloseBtn = styled.div`
-  @media(max-width: 591px) {
-    cursor: pointer;
-  }
-  @media(min-width: 591px) {
-    display: none;
-  }
-  @media(min-width: ${forDesktop.minWidth}px) {
-    display: none;
-  }
-`;
-const StyledMenu = styled(Menu)`
-  min-width: 20%;
-`;
-const LI = styled(Menu.Item)`
-  padding-left: 10px !important;
-  @media(max-width: ${forTablet.maxWidth}px) {
-    padding-right: 0px !important;
-  }
-`;
-const AlignBtn = styled.div`
-  margin-top: 20px;
-  @media(max-width: 591px) {
-    width: 90%;
-    display: flex;
-    justify-content: center;
-    padding-top: 30px;
-  }
-  @media(min-width: 592px) {
-    width: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-`;
-const AllBrandsTitle = styled.div`
-  @media(max-width: 591px) {
-    display: none;
-  }
-  @media(min-width: 592px) {
-    border-top: 7px solid white;
-    margin-top: 50px;
-    height: 60px;
-    width: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    h5{
-      font-size: 20px;
-    }
-  }
-  
-`;
-const GridFilterPart = styled.div`
-  @media(max-width: 591px) {
-    display: none;
-  }
-  @media(min-width: 592px) {
-    display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(80px, 1fr));
-    grid-column-gap: 10px;
-    grid-row-gap: 30px;
-  }
-`;
-const ContainerBrandsImg = styled.div`
-  text-align: center;
-  display: block;
-`;
-const WishList = styled.div`
-  margin-top: 50px;
-  height: 120px;
-  border-top: 7px solid white;
-  border-bottom: 7px solid white;
-  h5{
-    text-align: center;
-    font-size: 20px;
-    padding-top: 10px;
-    color: black;
-  }
-`;
-
-const NoWishItem = styled.div`
-  padding: 0px 5px 0px 5px;
-  p{
-    font-size: 12px;
-    text-align: center;
-    padding-top: 5px;
-  }
-`;
-const WishItem = styled.div`
-  padding: 0px 5px 0px 5px;
-  p{
-    font-size: 12px;
-    text-align: center;
-    padding-top: 5px;
-  }
-`;
-const ContainerImage = styled.div`
-  @media(max-width: 591px) {
-    display: none;
-  }
-  @media(min-width: 591px) {
-    text-align: center;
-    display: block;
-    img{
-      width: 100%;
-    }
-  }
-`;
 export default CatalogFilter
