748603981018cd71421582b7d68176c64bbac9dc
borislavski <v.borislavski@gmail.com>
2021-03-14 23:51:15 +0200

    add desktop version of Service section

--- src/components/ServiceSection/ServiceSection.jsx
+++ src/components/ServiceSection/ServiceSection.jsx
@@ -2,46 +2,45 @@ import React from 'react'
 import { Container } from '../common/Container'
 import { Wrapper } from '../common/Wrapper'
 import Item, {
-  Icon, Service, Description, Round, Img
+  Service, Description, Round, ItemWrapper
 } from './StyledServiceSection'
+import supportImg from '../../images/services/support.png'
+import accountImg from '../../images/services/account.png'
+import savingsImg from '../../images/services/savings.png'
 
 export default function ServiceSection() {
   return (
     <Container>
       <Wrapper>
-        <Item>
-          <Icon>
+        <ItemWrapper>
+          <Item>
             <Round>
-              <Img>1</Img>
+              <img src={supportImg} alt="headphones" />
             </Round>
-          </Icon>
-          <Service>Product Support</Service>
-          <Description>
-            Up to 3 years on-site warranty available for your peace of mind.
-          </Description>
-        </Item>
-        <Item>
-          <Icon>
+            <Service>Product Support</Service>
+            <Description>
+              Up to 3 years on-site warranty available for your peace of mind.
+            </Description>
+          </Item>
+          <Item>
             <Round>
-              <Img>1</Img>
+              <img src={accountImg} alt="user icon" />
             </Round>
-          </Icon>
-          <Service>Product Support</Service>
-          <Description>
-            Up to 3 years on-site warranty available for your peace of mind.
-          </Description>
-        </Item>
-        <Item>
-          <Icon>
+            <Service>Personal Account</Service>
+            <Description>
+              With big discounts, free delivery and a dedicated support specialist.
+            </Description>
+          </Item>
+          <Item>
             <Round>
-              <Img>1</Img>
+              <img src={savingsImg} alt="price tag" />
             </Round>
-          </Icon>
-          <Service>Product Support</Service>
-          <Description>
-            Up to 3 years on-site warranty available for your peace of mind.
-          </Description>
-        </Item>
+            <Service>Amazing Savings</Service>
+            <Description>
+              Up to 70% off new Products, you can be sure of the best price.
+            </Description>
+          </Item>
+        </ItemWrapper>
       </Wrapper>
     </Container>
   )
--- src/components/ServiceSection/StyledServiceSection.js
+++ src/components/ServiceSection/StyledServiceSection.js
@@ -1,25 +1,43 @@
 import styled from 'styled-components'
 
-const Item = styled.div`
-border: 1px solid #000000;
+export const ItemWrapper = styled.div`
+// border: 1px solid #C3082A;
+display: flex;
+flex-direction: row;
+justify-content: space-around;
+padding: 60px 0 45px;
 `;
 
-export const Icon = styled.div`
-
+const Item = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: space-around;
+align-items: center;
+// border: 1px solid #000000;
+width: 256px;
+height: 168px;
 `;
 
 export const Round = styled.div`
-`;
-
-export const Img = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+width: 65px;
+height: 65px;
+border-radius: 50%;
+background: #0156FF;
 `;
 
 export const Service = styled.div`
-
+font-size: 18px;
+font-weight: 700;
 `;
 
 export const Description = styled.div`
-
+text-align: center;
+font-size: 14px;
+font-weight: 400;
+line-height: 19px;
 `;
 
 export default Item
