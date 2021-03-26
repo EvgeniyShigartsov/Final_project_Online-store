cf909d9e708a359b3c035f7bfe1f128742a15213
Evgeniy Shigartsov <evgeniy.shigartsov2@gmail.com>
2021-03-26 04:37:10 +0200

    add Router page  seperately of app

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,10 @@
 import React, { useEffect } from 'react'
-import { Route, Switch } from 'react-router-dom'
 import { connect } from 'react-redux'
-import CreateCustomerPage from './components/CreateCustomerPage/CreateCustomerPage'
-import { BannerSlider } from './components/BannerSlider/BannerSlider'
-import { AboutUsPage } from './pages/About-us/AboutUs'
-import NewProductsSlider from './components/NewProductsSlider/NewProductsSlider'
 import Footer from './components/Footer/Footer'
-import ContactUsPage from './pages/Contact-us/ContactUs'
-import ProductPage from './components/ProductPage/ProductPage'
-import { HomepageBrands } from './components/BrandsAndFollow/HomePageBrands'
-import { HomepageFollowUs } from './components/BrandsAndFollow/FollowUsInstagram'
-import CatalogPage from './components/CatalogPage/CatalogPage'
 import Header from './components/Header/Header'
-import LogIn from './components/LogIn/LogIn'
-import Checkout from './components/Checkout/Checkout'
-import WishlistPage from './components/WishlistPage/WishlistPage'
 import { setWishlist } from './store/wishlist/middleware'
-import {CartPage} from './components/CartPage/CartPage'
-import OrderPage from './components/OrderPage/OrderPage'
 import ProductSubscribeModal from './components/ProductSubscribeModal/ProductSubscribeModal'
-import NoMatchPage from './components/NoMatchPage/NoMatchPage'
+import Router from './components/Router/Router'
 
 const App = connect(null, { setWishlist})(({ setWishlist }) => {
   useEffect(() => {
@@ -30,48 +15,7 @@ const App = connect(null, { setWishlist})(({ setWishlist }) => {
     <div>
       <Header />
       <ProductSubscribeModal />
-      <Switch>
-        <Route exact path="/">
-          <BannerSlider />
-          <NewProductsSlider />
-          <HomepageBrands />
-          <HomepageFollowUs />
-          {/* <MainCatalog /> */}
-        </Route>
-        <Route exact path="/signin">
-          <LogIn />
-        </Route>
-        <Route exact path="/signup">
-          <CreateCustomerPage />
-        </Route>
-        <Route exact path="/aboutus">
-          <AboutUsPage />
-        </Route>
-        <Route exact path="/contactus">
-          <ContactUsPage />
-        </Route>
-        <Route exact path="/cart">
-          <CartPage />
-        </Route>
-        <Route exact path="/checkout">
-          <Checkout />
-        </Route>
-        <Route exact path="/order">
-          <OrderPage />
-        </Route>
-        <Route exact path="/products/:itemNo">
-          <ProductPage />
-        </Route>
-        <Route exact path="/wishlist">
-          <WishlistPage />
-        </Route>
-        <Route exact path="/catalog">
-          <CatalogPage />
-        </Route>
-        <Route>
-          <NoMatchPage />
-        </Route>
-      </Switch>
+      <Router />
       <Footer />
     </div>
   )
