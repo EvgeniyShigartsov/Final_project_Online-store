0253a468329ec044cbd9c4930b2500c9aaf4268d
Evgeniy Shigartsov <evgeniy.shigartsov2@gmail.com>
2021-04-03 14:36:01 +0300

    fix trimStrimgValidator

--- src/utils/formTrimStringValidator.js
+++ src/utils/formTrimStringValidator.js
@@ -1,7 +1,12 @@
+/* eslint-disable prefer-promise-reject-errors */
 const formTrimStringValidator = (rejectMessage = 'The field should not contain just a spaces.') => () => {
-  const stringTrimCheck = (_, value) => (
-    value.trim() ? Promise.resolve() : Promise.reject(rejectMessage)
-  )
+  const stringTrimCheck = (_, value) => {
+    const trimmedStrimg = value.trim()
+    if (!trimmedStrimg) return Promise.reject(rejectMessage)
+    if (value[0] === ' ' || value[1] === ' ') return Promise.reject('Field should not contain just 1 character and spaces.')
+    
+    return Promise.resolve()
+  }
   return {
     validator: stringTrimCheck
   }
