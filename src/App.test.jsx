704049cda7c9bd23eac15f9584b91633180e610a
Evgeniy Shigartsov <evgeniy.shigartsov2@gmail.com>
2021-04-10 21:17:26 +0300

    delete duplicate code

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,27 +1,11 @@
 import React from 'react'
 import { HashRouter as Router } from 'react-router-dom'
 import { Provider } from 'react-redux'
+import render from 'enzyme/build/render'
 import { store } from './store/index'
 import App from './App'
-import render from 'enzyme/build/render'
 
 test('App render test', () => {
-  // Поскольку JSDOM не поддерживает, window.matchMedia
-  // В документации Jest теперь есть "официальный" обходной путь
-  Object.defineProperty(window, 'matchMedia', {
-    writable: true,
-    value: jest.fn().mockImplementation((query) => ({
-      matches: false,
-      media: query,
-      onchange: null,
-      addListener: jest.fn(), // Deprecated
-      removeListener: jest.fn(), // Deprecated
-      addEventListener: jest.fn(),
-      removeEventListener: jest.fn(),
-      dispatchEvent: jest.fn(),
-    })),
-  });
-
   render(
     <Provider store={store}>
       <Router>
