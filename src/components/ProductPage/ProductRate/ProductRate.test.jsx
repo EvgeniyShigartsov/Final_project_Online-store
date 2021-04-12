a80488b2e317372ec0fe847ec874f8b977cfe320
Evgeniy Shigartsov <evgeniy.shigartsov2@gmail.com>
2021-04-12 19:47:01 +0300

    add tests for product rate component & utils

--- src/components/ProductPage/ProductRate/ProductRate.test.jsx
+++ src/components/ProductPage/ProductRate/ProductRate.test.jsx
@@ -2,8 +2,15 @@
 import React from 'react'
 import { Provider } from 'react-redux'
 import { render } from '@testing-library/react'
+import axios from 'axios'
 import ProductRate from './ProductRate'
 import { store } from '../../../store/index'
+import { checkIsCustomerRatedProduct } from './utils/checkIsCustomerRateProduct'
+import { getUpdatedProductTeamplate } from './utils/getUpdatedProductTeamplate'
+import { mockCustomer } from '../../../mocks/customer'
+import { productMock } from '../../../mocks/products'
+import * as customerModule from '../../../store/customer/middleware'
+import * as productsModule from '../../../store/products/middleware'
 
 describe('Product page rate component test', () => {
   const props = {
@@ -11,7 +18,7 @@ describe('Product page rate component test', () => {
     productID: 'q1w2e3r4t5',
     itemNo: '1234567',
     updateOneProduct: jest.fn(),
-    isLogin: false,
+    isLogin: true,
     showAuthModal: jest.fn()
   }
 
@@ -40,5 +47,61 @@ describe('Product page rate component test', () => {
       </Provider>
     )
     expect(getAllByRole('radio').length).toBe(5)
+  })
+})
+
+describe('checkIsCustomerRatedProduct util test', () => {
+  const response = {
+    data: mockCustomer,
+    status: 200
+  }
+  test('check is checkIsCustomerRatedProduct util reutrns false with normal script', async () => {
+    customerModule.getCustomer = jest.fn().mockReturnValue(response)
+
+    const result = await checkIsCustomerRatedProduct('123456789')
+    expect(result).toBe(false)
+  })
+
+  test('check is checkIsCustomerRatedProduct util reutrns true if customer rated product before', async () => {
+    customerModule.getCustomer = jest.fn().mockReturnValue(response)
+    
+    const result = await checkIsCustomerRatedProduct(mockCustomer.ratedProducts[0])
+    expect(result).toBe(true)
+  })
+
+  test('check is checkIsCustomerRatedProduct util reutrns null when some troubles on server', async () => {
+    customerModule.getCustomer = jest.fn().mockReturnValue({
+      ...response,
+      status: 500,
+    })
+    
+    const result = await checkIsCustomerRatedProduct(mockCustomer.ratedProducts[0])
+    expect(result).toBe(null)
+  })
+})
+
+describe('getUpdatedProductTeamplate util test', () => {
+  const response = {
+    data: productMock,
+    status: 200,
+  }
+
+  test('check is getUpdatedProductTeamplate returns expected object', async () => {
+    productsModule.getOneProduct = jest.fn().mockReturnValue(response)
+    const expected = {
+      ...productMock,
+      reviews: [...productMock.reviews, 5]
+    }
+    const result = await getUpdatedProductTeamplate(productMock.itemNo, 5)
+    expect(result).toEqual(expected)
+  })
+
+  test('check is getUpdatedProductTeamplate returns null when some troubles on server', async () => {
+    productsModule.getOneProduct = jest.fn().mockReturnValue({
+      ...response,
+      status: 500,
+    })
+    const result = await getUpdatedProductTeamplate(productMock.itemNo, 5)
+    expect(result).toBe(null)
   })
 })
