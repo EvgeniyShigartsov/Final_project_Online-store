7d78ffd239a3b79017c356f823b79f54f8be32b5
Evgeniy Shigartsov <evgeniy.shigartsov2@gmail.com>
2021-04-12 16:04:13 +0300

    add product rate test

--- src/components/ProductPage/ProductRate/ProductRate.test.jsx
+++ src/components/ProductPage/ProductRate/ProductRate.test.jsx
@@ -1,1 +1,44 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react'
+import { Provider } from 'react-redux'
+import { render } from '@testing-library/react'
+import ProductRate from './ProductRate'
+import { store } from '../../../store/index'
+
+describe('Product page rate component test', () => {
+  const props = {
+    reviews: [1, 2, 3, 4, 5],
+    productID: 'q1w2e3r4t5',
+    itemNo: '1234567',
+    updateOneProduct: jest.fn(),
+    isLogin: false,
+    showAuthModal: jest.fn()
+  }
+
+  test('snapshot test', () => {
+    const { asFragment } = render(
+      <Provider store={store}>
+        <ProductRate {...props} />
+      </Provider>
+    )
+    expect(asFragment()).toMatchSnapshot()
+  })
+
+  test('should render a correct revievs quantity', () => {
+    const { getByText } = render(
+      <Provider store={store}>
+        <ProductRate {...props} />
+      </Provider>
+    )
+    expect(getByText(`Reviews (${props.reviews.length})`)).toBeDefined()
+  })
+
+  test('shuld have a five elems with "radio" role', () => {
+    const { getAllByRole } = render(
+      <Provider store={store}>
+        <ProductRate {...props} />
+      </Provider>
+    )
+    expect(getAllByRole('radio').length).toBe(5)
+  })
+})
